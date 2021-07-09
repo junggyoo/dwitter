152402616d124888d062053f5a9179513f7c8b65
eric <junggyoo.1@gmail.com>
2021-07-09 21:49:41 +0900

    fix: 서버 MVC 패턴 적용하여 리팩토링

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,79 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
-
-let tweets = [
-    {
-        id: '1',
-        text: 'Seize the world!',
-        createdAt: Date.now().toString(),
-        name: 'Eric',
-        username: 'eric',
-    },
-    {
-        id: '2',
-        text: '나는 밥이야',
-        createdAt: Date.now().toString(),
-        name: 'Bob',
-        username: 'bob',
-        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-];
+import * as tweetController from '../controller/tweet.js';
 
 const router = express.Router();
 
-// GET /tweets
-// GET /tweets?username=:username
-router.get('/', (req, res, next) => {
-    const username = req.query.username;
-    const data = username
-        ? tweets.filter((tweet) => tweet.username === username)
-        : tweets;
-    res.status(200).json(data);
-});
-
-// GET /tweets/:id
-router.get('/:id', (req, res, next) => {
-    const id = req.params.id;
-    const data = tweets.find((tweet) => tweet.id === id);
-    if (data) {
-        res.status(200).json(data);
-    } else {
-        res.status(404).json({ message: `Tweet id(${id}) not found` });
-    }
-});
-
-// POST /tweets
-router.post('/', (req, res, next) => {
-    const { text, name, username } = req.body;
-    const tweet = {
-        id: Date.now().toString(),
-        text,
-        createdAt: new Date(),
-        name,
-        username,
-    };
-    tweets = [tweet, ...tweets];
-    res.status(201).json(tweet);
-});
-
-// PUT /tweets/:id
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    const text = req.body.text;
-    const tweet = tweets.find((tweet) => tweet.id === id);
-    if (tweet) {
-        tweet.text = text;
-        res.status(200).json(tweet);
-    } else {
-        res.status(404).json({ message: `Tweet id(${id}) not found` });
-    }
-});
-
-// DELETE /tweets/:id
-router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    tweets = tweets.filter((tweet) => tweet.id !== id);
-    res.sendStatus(204);
-});
+router.get('/', tweetController.getTweets);
+router.get('/:id', tweetController.getTweet);
+router.post('/', tweetController.createTweet);
+router.put('/:id', tweetController.updateTweet);
+router.delete('/:id', tweetController.deleteTweet);
 
 export default router;
