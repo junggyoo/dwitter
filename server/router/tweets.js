e8d8547f6fd6716535169babf897ba85c433dc8e
eric <junggyoo.1@gmail.com>
2021-07-10 22:27:16 +0900

    feat: 서버 유효성 검사

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 import * as tweetController from '../controller/tweet.js';
 
 const router = express.Router();
 
-router.get('/', tweetController.getTweets);
+const validateTweet = [
+    body('text')
+        .ltrim()
+        .isLength({ min: 2 })
+        .withMessage('text should be at least 3 characters'),
+    validate,
+];
+
+router.get('/', [tweetController.getTweets]);
 router.get('/:id', tweetController.getTweet);
-router.post('/', tweetController.createTweet);
-router.put('/:id', tweetController.updateTweet);
+router.post('/', validateTweet, tweetController.createTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 router.delete('/:id', tweetController.deleteTweet);
 
 export default router;
