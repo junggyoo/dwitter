b761fff14462d637169e7cb61be61b490a5a3a3d
eric <junggyoo.1@gmail.com>
2021-07-13 12:26:53 +0900

    feat: 클라이언트 트윗 생성 소켓 구현

--- client/src/index.js
+++ client/src/index.js
@@ -9,22 +9,15 @@ import { AuthProvider } from './context/AuthContext';
 import { AuthErrorEventBus } from './context/AuthContext';
 import HttpClient from './network/http';
 import TokenStorage from './db/token';
-import socket from 'socket.io-client';
+import Socket from './network/socket.js';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 const tokenStorage = new TokenStorage();
 const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
 const authService = new AuthService(httpClient, tokenStorage);
-const tweetService = new TweetService(httpClient, tokenStorage);
-
-const socketIO = socket(baseURL);
-socketIO.on('connect_error', (error) => {
-  console.log('socket error', error);
-});
-socketIO.on('dwitter', (message) => {
-  console.log(message);
-});
+const socketClient = new Socket(baseURL, () => tokenStorage.getToken());
+const tweetService = new TweetService(httpClient, tokenStorage, socketClient);
 
 ReactDOM.render(
   <React.StrictMode>
