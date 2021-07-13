564a7d5a53edcab38dcd5df35d5647b3c822f1ac
eric <junggyoo.1@gmail.com>
2021-07-13 17:17:06 +0900

    feat: Auth 관련 MySQL 적용

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,36 +1,27 @@
-import bcrypt from 'bcrypt';
-
-const password = bcrypt.hashSync('skscjswo', 12);
-
-let users = [
-  {
-    id: '1',
-    username: 'bob',
-    password,
-    name: 'Bob',
-    email: 'bob@gmail.com',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-  {
-    id: '2',
-    username: 'ellie',
-    password,
-    name: 'Ellie',
-    email: 'ellie@gmail.com',
-    url: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
-  },
-];
+import { db } from '../db/datebase.js';
 
 export async function findByUserName(username) {
-  return users.find((user) => user.username === username);
+  return db
+    .execute('SELECT * FROM users WHERE username=?', [username])
+    .then((result) => {
+      return result[0][0];
+    });
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute('SELECT * FROM users WHERE id=?', [id]) //
+    .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
-  const create = { ...user, id: Date.now().toString() };
-  users.push(create);
-  return create.id;
+  const { username, password, name, email, url } = user;
+  return db
+    .execute(
+      'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
+      [username, password, name, email, url]
+    )
+    .then((result) => {
+      return result[0].inserId;
+    });
 }
