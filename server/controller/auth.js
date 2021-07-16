10e8da8cf91b3f5d1797b809c09f0742a8b76ea0
eric <junggyoo.1@gmail.com>
2021-07-16 10:31:18 +0900

    feat: 서버 XSS 어택 보안 세팅

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -37,6 +37,7 @@ export async function login(req, res) {
     }
 
     const token = createJwtToken(user.id);
+    setToken(res, token);
     res.status(200).json({ token, username });
 }
 
@@ -52,4 +53,14 @@ function createJwtToken(id) {
     return jwt.sign({ id }, config.jwt.secretKey, {
         expiresIn: config.jwt.expiresInSec,
     });
+}
+
+function setToken(res, token) {
+    const options = {
+        maxAge: config.jwt.expiresInSec * 1000,
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    };
+    res.cookie('token', token, options); // HTTP-ONLY
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,24 +5,30 @@ import { config } from '../config.js';
 const AUTH_ERROR = { message: 'Authentication Error' };
 
 export const isAuth = async (req, res, next) => {
-  const authHeader = req.get('Authorization');
-  if (!(authHeader && authHeader.startsWith('Bearer '))) {
-    return res.status(401).json(AUTH_ERROR);
-  }
-
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
-    if (error) {
-      return res.status(401).json(AUTH_ERROR);
+    let token;
+    const authHeader = req.get('Authorization');
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        token = authHeader.split(' ')[1];
+    }
+    if (!token) {
+        token = req.cookies['token'];
+    }
+    if (!token) {
+        return res.status(401).json(AUTH_ERROR);
     }
 
-    const user = await userRepository.findById(decoded.id);
-    if (!user) {
-      return res.status(401).json(AUTH_ERROR);
-    }
+    jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
+        if (error) {
+            return res.status(401).json(AUTH_ERROR);
+        }
 
-    req.userId = user.id;
-    req.token = token;
-    next();
-  });
+        const user = await userRepository.findById(decoded.id);
+        if (!user) {
+            return res.status(401).json(AUTH_ERROR);
+        }
+
+        req.userId = user.id;
+        req.token = token;
+        next();
+    });
 };
